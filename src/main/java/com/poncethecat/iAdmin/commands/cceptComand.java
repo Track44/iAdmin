5dbde7226ba9821d739fbf912a8317fbf9dfee88
T44Nadeko <T44Nadeko@users.noreply.github.com>
2017-01-11 17:34:44 +0000

    Create cceptComand.java
    :^)

---
+++ src/main/java/com/poncethecat/iAdmin/commands/cceptComand.java
@@ -0,0 +1,48 @@
+package com.poncethecat.iAdmin.commands;
+
+
+import com.darichey.discord.api.Command;
+import com.poncethecat.iAdmin.iAdmin;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RateLimitException;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class acceptCommand {
+    static String[] aliases1 = new String[]{""};
+    static Set<String> aliases = new HashSet(Arrays.asList(aliases1));
+    public static Command cmd = new Command("ccept").withAliases(aliases).caseSensitive(false).withDescription("The command for da noobs").onExecuted((context) -> {
+    String[] args = context.getArgs();
+    if(context.getMessage().getChannel().getID().equals("242809041003216897")) {
+        try {
+            context.getMessage().getAuthor().removeRole(context.getMessage().getGuild().getRolesByName("Newbie").get(0));
+            context.getMessage().delete();
+        } catch (MissingPermissionsException | RateLimitException | DiscordException e) {
+            e.printStackTrace();
+        }
+    } else {
+        return;
+    }
+
+    }).onFailure((context, err) -> {
+        try {
+            context.getMessage().reply("Something went wrong! D: Here's what happened: " + err.toString());
+        } catch (MissingPermissionsException | RateLimitException | DiscordException e) {
+
+            if(e instanceof MissingPermissionsException) {
+                try {
+                    iAdmin.client.getOrCreatePMChannel(context.getMessage().getGuild().getOwner()).sendMessage("Hey! Someone in your guild (" + context.getMessage().getAuthor().mention() + ") tried to use one of my commands, and for some reason I had permission to read the messages but I couldn't respond! \n Please kindly revoke my read permissions from that channel or allow me write permissions! Thanks :)");
+                } catch (MissingPermissionsException | RateLimitException | DiscordException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            e.printStackTrace();
+        }
+    });
+
+}
+
+
